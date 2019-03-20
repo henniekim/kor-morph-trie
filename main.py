4c92df65e54f03f370446b359e3b49f8ea983c1e
Dong Hyun Kim <32825120+henniekim@users.noreply.github.com>
2019-03-20 15:57:21 +0900

    main 함수 주석 추가

--- main.py
+++ main.py
@@ -10,20 +10,25 @@ if __name__ == '__main__':
 
     mytrie = Trie() # trie 객체 생성
 
-    # 불러온 사전을 Trie 구조로 인덱싱한다.
+    # ---------------------------------------- #
+    # STEP 1 : 불러온 사전을 Trie 구조로 인덱싱한다. #
+    # ---------------------------------------- #
+
     for i in range (myDict.numberOfContents) :
-        #print(myDict.contents[i]) # 불러온 단어 출력
-        mySlice = myDict.contents[i].split('\t') # 불러온 단어를 column 별로 자른다. :
-        # 단어 | 품사  | 품사코드
-        # 가  | 보조사 | JKG
+        mySlice = myDict.contents[i].split('\t')
+        # 불러온 단어를 column 별로 자른다.
+        # -------------------- #
+        # 단어 | 품사  | 품사코드  #
+        # 가  | 보조사 | JKG     #
+        # -------------------- #
         myWord = mySlice[0]
         myData = mySlice[1]
         mytrie.add(myWord, myData)
 
+    print(" TRIE 사전 구축을 완료하였습니다. ! \n")
     # Trie 인덱싱 결과
     # print(mytrie)
-
-    # Trie 검색 결과
+    # plain 사전 text 파일을 이용하여 TRIE 구조를 가지는 사전 구축 완료 ! #
 
     # key=input("분석 문장을 입력하세요.\n")
     key = '분노를 참는 것이 사람의 슬기이며 남의 허물을 덮어 주는 것이 자기의 영광이다'
@@ -43,6 +48,9 @@ if __name__ == '__main__':
             for j in range(lenSyllable + 1):
                 table[i].append([])
 
+        # ----------------------------------------------- #
+        # STEP 2 : Tabluar Parsing을 이용하여 형태소 분석 시작  #
+        # ----------------------------------------------- #
 
         for i in range (0, lenSyllable):
             for j in range (0,lenSyllable):
@@ -57,79 +65,42 @@ if __name__ == '__main__':
                         table[i][j+1].append(currentData)
                 else :
                     table[i][j+1].extend(currentData)
-
-                # if currentData is not None :
-                #     currentData = currentData.split(',') # 데이터가 여러개일 경우 인덱싱
-                # table[i][j+1].extend(currentData)
-                # else :
-                #     pass#table[i][j+1].append()
-                # else :
-                #     table[i][j+1] = None
-                #     # table.append('({},{}), 단어 : {}, 품사 : {}'.format(i,j+1,currentWord, currentData))
-        #print(table)
+        # print(table)
+        print('Tablular parsing 1차 passing 완료 ')
         # 여기가 tabular parsing 완성! - 1차 passing
-
 
         grammar =[]
         for i in range (0,lenSyllable+1):
             for j in range (0, lenSyllable):
 
-                if table[i][j] != []: # TODO 단어에 의미가 여러개 있을 경우도 생각해줘야 한다
+                if table[i][j] != []:
                     grammar = table[i][j]
-                    # if '' in grammar :
-                    #     grammar = grammar.split(' ')
 
+                    # 단어에 의미가 여러개 있을 경우도 생각해줘야 한다
                     for numOfMean in range (len(grammar)) :
                         grammarLoop = grammar[numOfMean]
                         for k in range (j+1, lenSyllable+1):
                             if table[j][k] != []:
                                 currentGrammar = table[j][k]
-                                # currentGrammar = currentGrammar[0] # currentGrammar에도 뜻이 여러개 있을 수도 있다.
-                                # if currentGrammar != [] :
-                                #     currentGrammar = currentGrammar.split(' ')
-
+                                # currentGrammar에도 뜻이 여러개 있을 수도 있다.
                                 for means in range (len(currentGrammar)):
                                     resultGrammar = ''
                                     currentGrammarSeg = currentGrammar[means]
                                     resultGrammar += resultGrammar + grammarLoop + ' '+currentGrammarSeg+' '
                                     resultGrammar = resultGrammar[:-1]
                                     table[i][k].extend([resultGrammar])
-
-                # else :
-                #     pass
-
-
-
         #print(table)
         printWord = synWord[SynWordIdx]
-        # print(str(printWord[2:3])+str(table[2][3]))
 
+        # ------------------------------------------- #
+        # STEP 3 : 문법 정보를 이용하여 결과를 출력하는 단계  #
+        # ------------------------------------------- #
         for i in range(0, lenSyllable) :
             for j in range(1, lenSyllable+1) :
                 currentTable = table[i][j]
 
                 for k in range (len(currentTable)):
 
-                    if currentTable[k] in myGrammar.contents : # 사전에서 검색해서 있을 경우 결과 출력
+                    # 문법 정보를 검색해서 가능한 문법일 경우 결과 출력
+                    if currentTable[k] in myGrammar.contents :
                         print(printWord[i:j]+'/'+str(currentTable[k]))
-                    # else : # 검색 결과가 없을 경우 결과를 테이블에서 지운다.
-                        # table[i][j] = []
-
-
-
-        #print(table)
-
-
-
-    # TODO : 좌우 접속 정보 가지고 형태소 분석하기 !
-
-
-    # item = mytrie.getData(key)
-    # try :
-    #     # 단어를 찾고 의미 중복이 있을 수 있으므로 ','로 분리한다
-    #     item = item.split(',')
-    #     print(item)
-    #
-    # except :
-    #     # 찾는 단어가 없을 경우 예외 처리
-    #     pass
